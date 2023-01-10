bdd9b9fb2a22ada020a23d3f0026d78f254ecbe6
Averexs <108954427+Averexs@users.noreply.github.com>
2023-01-09 21:07:38 -0500

    Delete update-crafty.sh

--- update-crafty.sh
+++
@@ -1,7 +0,0 @@
-sudo systemctl stop crafty
-cd /var/opt/minecraft/crafty/crafty-4
-sudo su crafty
-git reset --hard origin/master
-cd /var/opt/minecraft/crafty
-./update_crafty.sh
-sudo systemctl start crafty
