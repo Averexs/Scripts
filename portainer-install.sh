93ba4ba15c40357ef839b119f8828c53cc4b2546
Averexs <108954427+Averexs@users.noreply.github.com>
2023-05-24 22:40:14 -0400

    Updated portainer-install.sh

--- portainer-install.sh
+++ portainer-install.sh
@@ -1,2 +1,4 @@
+ #!/bin/bash
+
 docker pull portainer/portainer-ce:latest
 docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /var/docker/portainer:/data portainer/portainer-ce:latest
